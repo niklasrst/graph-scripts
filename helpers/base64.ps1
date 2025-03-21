d6379176cedf9cbc0ad3cec3c915c8d971e84a80
Niklas Rast <177048868+niklasrst@users.noreply.github.com>
2025-03-21 14:32:51 +0100

    feat(base64): added cbh and params

--- helpers/base64.ps1
+++ helpers/base64.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+   Encode or decode a string to/from base64.
+
+.DESCRIPTION
+    This script allows you to encode or decode a string to/from base64.
+
+.PARAMETER -encode
+    Encode a string to base64.
+
+.PARAMETER -decode
+    Decode a base64 string.
+
+.PARAMETER -Verbose
+   Enable verbose output.
+
+.EXAMPLE
+   .\base64.ps1 -direction encode -string "Hello World"
+   .\base64.ps1 -direction decode -string "SGVsbG8gV29ybGQ="
+
+.LINK
+   https://github.com/niklasrst/graph-scripts/tree/main/helpers/
+
+.AUTHOR
+   Niklas Rast
+#>
+
 param (
     [Parameter(Mandatory=$true, HelpMessage='Define if you want to encode or decode base64')]
     [ValidateSet("encode", "decode")]
